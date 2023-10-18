fd7a08f3965afb7dbaa2d52d930348bc5a87653a
Elena Serebrennikova <eserebrennikovaa@mail.ru>
2023-10-18 09:30:28 +0300

    add Ex5 project

---
+++ Ex/Ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()== "пирожок")
+{
+    Console.WriteLine("Ура, это же Чубася");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
